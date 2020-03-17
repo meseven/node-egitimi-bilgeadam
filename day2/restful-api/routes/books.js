cacf803e8e182c1b5afcd302c0df57a9ff78463b
Mehmet Seven <mehmetseven0@gmail.com>
2020-03-17 11:29:10 +0300

    [POST] /book

--- day2/restful-api/routes/books.js
+++ day2/restful-api/routes/books.js
@@ -16,27 +16,25 @@ router.post("/", function(req, res, next) {
 	// });
 
 	// v.2
-	// const { title, author, year } = req.body;
-
-	// const book = new Book({
-	// 	title,
-	// 	author,
-	// 	year
-	// });
-
-	console.log(...req.body);
+	const { title, author, year } = req.body;
 
 	const book = new Book({
-		...req.body
+		title,
+		author,
+		year
 	});
+
+	// v3
+	// const book = new Book({
+	// 	...req.body
+	// });
 
 	book
 		.save()
-		.then(() => console.log("Kayıt eklendi."))
-		.catch(e => {
-			console.log(e);
-			console.log("Kayıt eklenemedi!");
-		});
+		.then(data => res.json(data))
+		.catch(e =>
+			res.json({ error: { statusCode: 1, message: "Kayıt eklenemedi." } })
+		);
 
 	// res.json({ name: "franz kafka - dönüşüm" });
 });
