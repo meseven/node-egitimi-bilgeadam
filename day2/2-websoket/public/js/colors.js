4adbfba075e54576584c8ff7f83883d3dccac1bc
Mehmet Seven <mehmetseven0@gmail.com>
2020-03-18 09:02:06 +0300

    colors pallette

--- day2/2-websoket/public/js/colors.js
+++ day2/2-websoket/public/js/colors.js
@@ -1,3 +1,19 @@
 $(() => {
 	const socket = io.connect("http://localhost/");
+
+	const defaultColor = localStorage.getItem("bgcolor");
+	$("#colors li").removeClass("active");
+	$(".container").css("background-color", defaultColor);
+	$(`#colors li[data-color='${defaultColor}']`).addClass("active");
+
+	$("#colors li").on("click", function() {
+		const color = $(this).data("color");
+
+		$(".container").css("background-color", color);
+
+		$("#colors li").removeClass("active");
+		$(this).addClass("active");
+
+		localStorage.setItem("bgcolor", color);
+	});
 });
