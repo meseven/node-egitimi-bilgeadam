40b862ab8d0bf443db43c80b2e7425115290bde3
Mehmet Seven <mehmetseven0@gmail.com>
2020-03-18 09:46:31 +0300

    socketio

--- day2/2-websoket/app.js
+++ day2/2-websoket/app.js
@@ -9,6 +9,8 @@ const io = require("socket.io")(server);
 server.listen(80);
 
 io.on("connection", function(socket) {
+	console.log(socket.id);
+
 	socket.on("newUser", data => {
 		socket.broadcast.emit("newUser", data);
 	});
