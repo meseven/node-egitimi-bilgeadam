6605ac7cf15dbcf5e00c04f40e7203b7ebbd2caa
Mehmet Seven <mehmetseven0@gmail.com>
2020-03-18 13:10:49 +0300

    test env

--- day2/restful-api/app.js
+++ day2/restful-api/app.js
@@ -1,40 +1,40 @@
-const express = require("express");
-const path = require("path");
-const favicon = require("serve-favicon");
-const logger = require("morgan");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
 //db
-require("dotenv").config();
-require("./db")();
+require('dotenv').config();
+require('./db')();
 
-const index = require("./routes/index");
-const books = require("./routes/books");
+const index = require('./routes/index');
+const books = require('./routes/books');
 
 const app = express();
 
 // middleware
-const verifyToken = require("./middleware/verify-token");
+const verifyToken = require('./middleware/verify-token');
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", index);
-app.use("/books", verifyToken, books);
+app.use('/', index);
+app.use('/books', verifyToken, books);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-	const err = new Error("Not Found");
+	const err = new Error('Not Found');
 	err.status = 404;
 	next(err);
 });
@@ -43,11 +43,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
-	res.locals.error = req.app.get("env") === "development" ? err : {};
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
 	// render the error page
 	res.status(err.status || 500);
-	res.render("error");
+	res.render('error');
 });
 
 module.exports = app;
